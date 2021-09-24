ffe6b0ed028bdbf027849b4b27f51cfde3b06ce1
Nikita Kornilov <kornilov.nm@gmail.com>
2021-09-25 05:06:56 +1000

    Small Input fix

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import styled from "styled-components"
 import { FieldError } from "react-hook-form"
 
@@ -9,10 +9,8 @@ const InputWrapper = styled.div`
 const StyledInput = styled.input`
   width: 100%;
   padding: 8px 0 8px 10px;
-
   font-size: 14px;
   line-height: 171%;
-
   background-color: #fff;
   color: #000;
   border: 1px solid ${(props) => props.theme.gray800};
@@ -23,17 +21,20 @@ const StyledInput = styled.input`
   &::placeholder {
     color: ${(props) => props.theme.gray900};
   }
+
   &:focus {
     border-color: ${(props) => props.theme.gray800};
   }
+
   &:hover {
     border-color: #000;
   }
+
   &:disabled &::placeholder {
     color: ${(props) => props.theme.grayText};
     border-color: ${(props) => props.theme.grayText};
   }
-  
+
   &.error {
     border-color: ${(props) => props.theme.red};
   }
@@ -41,21 +42,18 @@ const StyledInput = styled.input`
 
 const InputTitle = styled.div`
   margin-bottom: 8px;
-
   font-size: 14px;
   font-weight: 500;
   line-height: 171%;
-
   color: #000;
 `
 
 const InputError = styled.p`
   margin-top: 4px;
-
   font-size: 10px;
   line-height: 120%;
-
   color: ${(props) => props.theme.red};
+
   @media (min-width: 768px) {
     font-size: 12px;
     line-height: 155%;
@@ -67,11 +65,10 @@ interface IInputProps {
   name: string
   onChange: (value: never) => void
   onBlur: () => void
-  isDirty: boolean
   value: string
   invalid: boolean
-  isTouched: boolean
   error?: FieldError
+  disabled?: boolean
 }
 
 const Input: React.FC<IInputProps> = ({
@@ -82,23 +79,21 @@ const Input: React.FC<IInputProps> = ({
   invalid,
   error,
   title,
+  disabled,
 }) => {
   return (
     <InputWrapper>
       <InputTitle>{title}</InputTitle>
-
       <StyledInput
         name={name}
         onBlur={onBlur}
         onChange={onChange}
         value={value}
+        disabled={disabled}
       />
-
       {invalid ? (
-        <InputError>{error?.message}</InputError>
-      ) : (
-        ""
-      )}
+        <InputError>{error?.message || "Введите верное значение"}</InputError>
+      ) : null}
     </InputWrapper>
   )
 }
